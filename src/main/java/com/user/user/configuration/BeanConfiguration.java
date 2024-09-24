57220887073e0118f3276776dbe56a6defaa95c0
juanrodriguez9 <juansebastianrodriguez904@gmail.com>
2024-09-24 01:42:28 -0500

    cconfiguracion de inyeccion de componentes BeanConfiguration

--- src/main/java/com/user/user/configuration/BeanConfiguration.java
+++ src/main/java/com/user/user/configuration/BeanConfiguration.java
@@ -1,4 +1,28 @@
 package com.user.user.configuration;
 
+import com.user.user.adapters.driven.jpa.mysql.adapter.UserAdapter;
+import com.user.user.adapters.driven.jpa.mysql.mapper.IUserEntityMapper;
+import com.user.user.adapters.driven.jpa.mysql.repository.IUserRepository;
+import com.user.user.domain.api.IUserServicePort;
+import com.user.user.domain.api.usecase.UserUseCase;
+import com.user.user.domain.spi.IUserPersistencePort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
 public class BeanConfiguration {
+    private final IUserRepository userRepository;
+    private final IUserEntityMapper userEntityMapper;
+
+    @Bean
+    public IUserPersistencePort userPersistencePort(){
+        return new UserAdapter(userRepository, userEntityMapper);
+    }
+
+    @Bean
+    public IUserServicePort userServicePort(){
+        return new UserUseCase(userPersistencePort());
+    }
 }
